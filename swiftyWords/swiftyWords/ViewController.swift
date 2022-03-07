27214e7b261b5bb0840f7eb6f9ebbc088c3b59c7
SunilKumarDevalla <sunilkumardevalla@gmail.com>
2022-03-07 08:08:22 -0500

    Add basic file structure.

---
+++ swiftyWords/swiftyWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftyWords
+//
+//  Created by Sunny on 3/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
